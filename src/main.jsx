ef1c4ddcea12e9ce850c9b74356a7fca2de9e257
Foridul35962 <foridulislam35962@gmail.com>
2025-12-09 18:28:59 +0600

    setup interceptors

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { setupInterceptors } from './components/refreshToken/axios.js'
+
+setupInterceptors(store)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
