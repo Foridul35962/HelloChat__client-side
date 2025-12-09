236180c021190c3bb4f12fefe233c77cc9e3269a
Foridul35962 <foridulislam35962@gmail.com>
2025-12-09 13:53:54 +0600

    axios instance created

---
+++ src/components/refreshToken/axios.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { setCredentials, forceLogout } from '../../store/slices/authSlice';
+import store from '../../store/store';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state?.auth?.accessToken;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (!error.response) {
+      return Promise.reject(error);
+    }
+
+    if (originalRequest.url.includes('/forceLogout')) {
+      store.dispatch(forceLogout());
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await axios.post(
+          `${import.meta.env.VITE_SERVER_URL}/api/v1/refresh-token`,
+          {},
+          { withCredentials: true }
+        );
+
+        const newAccessToken = res.data.accessToken;
+
+        store.dispatch(
+          setCredentials({
+            accessToken: newAccessToken,
+            user: store.getState().auth.user,
+          })
+        );
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        console.error('Refresh failed → Force forceLogout');
+        store.dispatch(forceLogout());
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
