6a8ae2afe2b4aff52e8056ba33df0ec7b98cc460
Foridul35962 <foridulislam35962@gmail.com>
2025-12-11 01:55:36 +0600

    contact functionality added

---
+++ src/components/layout/ContactList.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import LoadingSkeleton from '../common/LoadingSkeleton'
+import avatar from '../../assets/avatar.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllContacts } from '../../store/slices/messageSlice'
+
+const ContactList = () => {
+  const dispatch = useDispatch()
+  const { allContacts, loading } = useSelector((state) => state.message)
+  useEffect(() => {
+      const showContacts = async () => {
+        try {
+          await dispatch(getAllContacts()).unwrap()
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      showContacts()
+    }, [dispatch])
+  return (
+    <>
+      {
+        loading ? <LoadingSkeleton /> : allContacts &&
+          allContacts.map((contacts, idx) => (
+            <div key={idx} className='flex flex-col gap-2 text-white'>
+              <div className='flex gap-2 items-center bg-gray-900 rounded-xl px-1 py-1.5'>
+                <img src={contacts?.profilePic?.url || avatar} alt="img" className='size-10 rounded-full object-cover' />
+                <p className='font-semibold'>{contacts?.fullName}</p>
+              </div>
+            </div>
+          ))
+      }
+    </>
+  )
+}
+
+export default ContactList
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setActiveTab } from '../store/slices/messageSlice'
 import ProfileHeader from '../components/common/ProfileHeader'
 import { LoaderIcon } from 'lucide-react'
+import ChatList from '../components/layout/ChatList'
+import ContactList from '../components/layout/ContactList'
 
 const Home = () => {
   const dispatch = useDispatch()
@@ -19,11 +21,14 @@ const Home = () => {
               {/* profile header */}
               <ProfileHeader />
 
-              <div className='border-2 border-cyan-400 rounded-2xl'>
-                <div className='flex justify-between gap-2 w-full p-3'>
+              <div className='flex flex-col gap-3 border-2 border-cyan-400 rounded-2xl p-3'>
+                <div className='flex justify-between gap-2 w-full'>
                   <button className={`${activeTab === 'chats' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`} onClick={() => dispatch(setActiveTab('chats'))}>Chats</button>
                   <button className={`${activeTab === 'contacts' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`} onClick={() => dispatch(setActiveTab('contacts'))}>Contacts</button>
                 </div>
+                {
+                  activeTab === 'chats' ? <ChatList activeTab={activeTab}/> : <ContactList />
+                }
               </div>
             </div>
             {/* right side */}
