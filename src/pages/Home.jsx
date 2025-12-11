0e60260c4a070febc1663b59ac4815f314826345
Foridul35962 <foridulislam35962@gmail.com>
2025-12-12 02:27:10 +0600

    responsive design added for contact and chats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,35 +5,47 @@ import ProfileHeader from '../components/common/ProfileHeader'
 import { LoaderIcon } from 'lucide-react'
 import ChatList from '../components/layout/ChatList'
 import ContactList from '../components/layout/ContactList'
+import Conversation from '../components/layout/Conversation'
+import NoConversationPlaceholder from '../components/common/NoConversationPlaceholder'
 
 const Home = () => {
   const dispatch = useDispatch()
-  const { user, loading, } = useSelector((state) => state.auth)
-  const { activeTab } = useSelector((state) => state.message)
+  const { user } = useSelector((state) => state.auth)
+  const { activeTab, selectedUser } = useSelector((state) => state.message)
 
   return (
     <>
       {
         !user ? <LoaderIcon className="animate-spin size-20 text-white" /> :
-          <div className='bg-gray-800 rounded-2xl h-full w-full p-8 flex gap-2'>
+          <div className='bg-gray-800 rounded-2xl h-[86vh] w-full p-2 md:p-8 flex gap-2'>
             {/* left side */}
-            <div className='hidden w-1/3 sm:flex flex-col gap-2.5'>
+            <div className={`${selectedUser && 'hidden md:flex w-full'} w-full md:w-1/3 flex flex-col gap-2.5 overflow-y-scroll custom-scrollbar`}>
               {/* profile header */}
               <ProfileHeader />
 
               <div className='flex flex-col gap-3 border-2 border-cyan-400 rounded-2xl p-3'>
                 <div className='flex justify-between gap-2 w-full'>
-                  <button className={`${activeTab === 'chats' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`} onClick={() => dispatch(setActiveTab('chats'))}>Chats</button>
-                  <button className={`${activeTab === 'contacts' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`} onClick={() => dispatch(setActiveTab('contacts'))}>Contacts</button>
+                  <button
+                    className={`${activeTab === 'chats' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`}
+                    onClick={() => dispatch(setActiveTab('chats'))}
+                  >
+                    Chats
+                  </button>
+                  <button
+                    className={`${activeTab === 'contacts' && 'bg-cyan-900 font-semibold'} text-white cursor-pointer w-full rounded py-1`}
+                    onClick={() => dispatch(setActiveTab('contacts'))}
+                  >
+                    Contacts
+                  </button>
                 </div>
                 {
-                  activeTab === 'chats' ? <ChatList activeTab={activeTab}/> : <ContactList />
+                  activeTab === 'chats' ? <ChatList activeTab={activeTab} /> : <ContactList />
                 }
               </div>
             </div>
             {/* right side */}
-            <div className='w-full sm:w-2/3'>
-              hi
+            <div className={`w-2/3 md:pl-6 md:border-l md:border-gray-600 ${selectedUser? 'block w-full md:w-2/3' : 'hidden md:block'}`}>
+              {selectedUser ? <Conversation /> : <NoConversationPlaceholder />}
             </div>
           </div>}
     </>
