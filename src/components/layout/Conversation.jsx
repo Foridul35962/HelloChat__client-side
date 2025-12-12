1eade42735f4e366b07e55a10ff664660352d767
Foridul35962 <foridulislam35962@gmail.com>
2025-12-13 02:17:12 +0600

    conversation fully done

--- src/components/layout/Conversation.jsx
+++ src/components/layout/Conversation.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import ChatHeader from '../common/ChatHeader'
 import ChatContent from '../common/ChatContent'
 import SendMessage from '../common/SendMessage'
+import { getMessagesByUserId } from '../../store/slices/messageSlice'
+import NoChatHistoryPlaceholder from '../common/NoChatHistoryPlaceholder'
+import { LoaderIcon } from 'lucide-react'
 
 const Conversation = () => {
     const dispatch = useDispatch()
-    const { selectedUser } = useSelector((state) => state.message)
+    const { selectedUser, messages, isMessagesLoading } = useSelector((state) => state.message)
+    useEffect(() => {
+        const getMessage = async () => {
+            try {
+                await dispatch(getMessagesByUserId(selectedUser?._id)).unwrap()
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getMessage()
+    }, [dispatch, selectedUser])
     return (
         <>
             {
@@ -16,7 +29,15 @@ const Conversation = () => {
                             <ChatHeader />
                         </div>
                         <div className="flex-1 overflow-y-auto">
-                            <ChatContent />
+                            {
+                                isMessagesLoading ?
+                                    <div className='w-full h-full flex items-center justify-center'>
+                                        <LoaderIcon className="animate-spin size-20 text-white" />
+                                    </div> :
+                                    messages.length !== 0 ?
+                                        <ChatContent messages={messages} /> :
+                                        <NoChatHistoryPlaceholder name={selectedUser?.fullName} />
+                            }
                         </div>
                         <div>
                             <SendMessage />
