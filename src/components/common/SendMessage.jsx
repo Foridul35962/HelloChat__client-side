1bdc31b88600654de706a597e0dd6d7a47611ff3
Foridul35962 <foridulislam35962@gmail.com>
2025-12-13 02:16:41 +0600

    message send successfully

--- src/components/common/SendMessage.jsx
+++ src/components/common/SendMessage.jsx
@@ -1,7 +1,13 @@
-import { Image, Send } from 'lucide-react';
+import { Image, LoaderIcon, Send } from 'lucide-react';
 import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify'
+import { sendMessage } from '../../store/slices/messageSlice';
 
 const SendMessage = () => {
+  const dispatch = useDispatch()
+  const { selectedUser, sendMessageLoading } = useSelector((state) => state.message)
+
   const [message, setMessage] = useState('');
   const [image, setImage] = useState(null);
   const fileInputRef = useRef(null);
@@ -15,9 +21,18 @@ const SendMessage = () => {
     if (file) setImage(file);
   };
 
-  const handleSend = () => {
+  const handleSend = async () => {
     if (!message && !image) return;
-    console.log('Sending:', { message, image });
+
+    const formData = new FormData()
+    formData.append('image', image)
+    formData.append('message', message)
+
+    try {
+      await dispatch(sendMessage({ id: selectedUser._id, message: formData})).unwrap()
+    } catch (error) {
+      toast.error(error)
+    }
 
     // Reset input
     setMessage('');
@@ -27,44 +42,45 @@ const SendMessage = () => {
 
   return (
     <div className='bg-gray-900 flex w-full gap-2 p-2 items-center rounded-lg'>
-  {/* Text Input */}
-  <input
-    type="text"
-    name="message"
-    value={message}
-    onChange={(e) => setMessage(e.target.value)}
-    placeholder='Type a message...'
-    className='flex-1 min-w-0 bg-gray-800 text-white rounded-full px-3 py-2 outline-none'
-  />
+      {/* Text Input */}
+      <input
+        type="text"
+        name="message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => { if (e.key === 'Enter') handleSend() }}
+        placeholder='Type a message...'
+        className='flex-1 min-w-0 bg-gray-800 text-white rounded-full px-3 py-2 outline-none'
+      />
 
-  {/* Buttons */}
-  <div className='flex gap-2 shrink-0'>
-    {/* Image Button */}
-    <button
-      type="button"
-      onClick={handleFileClick}
-      className='p-2 bg-cyan-500 text-white rounded-full hover:bg-cyan-600'
-    >
-      <Image className='size-4' />
-    </button>
-    <input
-      type="file"
-      name="image"
-      ref={fileInputRef}
-      onChange={handleFileChange}
-      className='hidden'
-    />
+      {/* Buttons */}
+      <div className='flex gap-2 shrink-0'>
+        {/* Image Button */}
+        <button
+          type="button"
+          onClick={handleFileClick}
+          className='p-2 bg-cyan-500 text-white rounded-full hover:bg-cyan-600'
+        >
+          {image ? <p>loaded</p> : <Image className='size-4' />}
+        </button>
+        <input
+          type="file"
+          name="image"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          className='hidden'
+        />
 
-    {/* Send Button */}
-    <button
-      type="button"
-      onClick={handleSend}
-      className='p-2 bg-green-500 text-white rounded-full hover:bg-green-600'
-    >
-      <Send className='size-4' />
-    </button>
-  </div>
-</div>
+        {/* Send Button */}
+        <button
+          type="button"
+          onClick={handleSend}
+          className='p-2 bg-green-500 text-white rounded-full hover:bg-green-600'
+        >
+          {sendMessageLoading ? <LoaderIcon className="animate-spin size-4" /> :<Send className='size-4' />}
+        </button>
+      </div>
+    </div>
 
   );
 };
